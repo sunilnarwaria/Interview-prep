d49de02ce11e82d0dc58bea52b9010926092d1ec
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-09-24 13:15:18 +0530

    Create find the duplicate number.cpp

---
+++ find the duplicate number.cpp
@@ -0,0 +1,15 @@
+// similarly find cycle node in linkedlist
+
+ int findDuplicate(vector<int>& nums) {
+         int slow = 0, fast = 0;
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }while(slow != fast);
+    slow = 0;
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+    }
